268b4bbadcac35235e7345ee0ac4f36e3721d566
SultanEsen <beksultanesenkulov@Air--Beksultan.mshome.net>
2024-09-10 17:39:40 +0600

    updated

--- ua.py
+++ ua.py
@@ -1,10 +1,15 @@
 import sqlite3
 import pandas as pd
 
-con = sqlite3.connect('ua.db')
-wb = pd.read_excel('ua_file.xls', sheet_name=None)
+# con = sqlite3.connect('ua3.db')
+# wb = pd.read_excel('ua2.xls', sheet_name='Реєстр', usecols=[0, 1, 2, 3, 4, 5, 9])
+
+con = sqlite3.connect('ua3.db')
+wb = pd.read_excel('ua2.xls', sheet_name=None, usecols=[0, 1, 2, 3, 4, 5, 9])
 
 for sheet in wb:
     wb[sheet].to_sql(sheet, con, index=False)
 con.commit()
 con.close()
+
+print(wb)
